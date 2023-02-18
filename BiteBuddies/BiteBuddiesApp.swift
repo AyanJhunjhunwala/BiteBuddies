16e85b3fbc16983158513a9ffba840bb3f992f17
AyanJhunjhunwala <84145352+AyanJhunjhunwala@users.noreply.github.com>
2023-02-18 00:57:51 -0800

    Initial Commit

---
+++ BiteBuddies/BiteBuddiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiteBuddiesApp.swift
+//  BiteBuddies
+//
+//  Created by Ayan Jhunjhunwala on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct BiteBuddiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
