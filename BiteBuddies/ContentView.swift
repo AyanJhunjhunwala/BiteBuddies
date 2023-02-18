db9ccde555a0a1f15a7dfe90eac0d78acd1ed3e2
AyanJhunjhunwala <84145352+AyanJhunjhunwala@users.noreply.github.com>
2023-02-18 11:31:24 -0800

    LGTM

--- BiteBuddies/ContentView.swift
+++ BiteBuddies/ContentView.swift
@@ -70,97 +70,97 @@ struct User: Identifiable {
     var id = UUID()
     var name: String
 }
-struct LoggedInView: View {
-    @State private var foodImage: UIImage?
-    
-    var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: "person.crop.circle.fill")
-                    .resizable()
-                    .frame(width: 40, height: 40)
-                    .foregroundColor(.white)
-                    .padding()
-                Text("Cooking Together")
-                    .font(.largeTitle)
-                    .foregroundColor(.white)
-                    .padding()
-                Spacer()
-                Image(systemName: "camera.fill")
-                    .resizable()
-                    .frame(width: 25, height: 30)
-                    .foregroundColor(.white)
-                    .padding()
-                    .onTapGesture {
-                        // Add functionality for camera button
-                    }
-            }
-            .padding(.horizontal)
-            .padding(.top, 20)
-            
-            ScrollView {
-                VStack(spacing: 20) {
-                    ForEach(0..<5) { index in
-                        HStack {
-                            
-                            Image("FoodImgs")
-                                .resizable()
-                                .frame(width: 50, height: 50)
-                                .foregroundColor(.white)
-                                .padding()
-                            VStack(alignment: .leading) {
-                                Text("Cooking Event \(index)")
-                                    .font(.title2)
-                                    .fontWeight(.bold)
-                                    .foregroundColor(.white)
-                                Text("Join us for a fun evening of cooking and socializing with other foodies in the area! All skill levels welcome. Please let us know if you have any dietary restrictions or preferences.")
-                                    .font(.body)
-                                    .foregroundColor(.white)
-                            }
-                            Spacer()
-                            Button(action: {
-                                print("Join Event \(index) tapped")
-                            }) {
-                                Text("Join")
-                                    .foregroundColor(.white)
-                                    .padding(.horizontal, 10)
-                                    .padding(.vertical, 5)
-                                    .background(Color.blue)
-                                    .cornerRadius(10)
-                            }
-                            .padding(.trailing, 10)
-                        }
-                        .frame(maxWidth: .infinity)
-                        .background(Color(red: 50/255, green: 50/255, blue: 50/255))
-                        .cornerRadius(10)
-                    }
-                }
-                .padding(.horizontal)
-                .padding(.bottom, 20)
-            }
-            
-            if let foodImage = foodImage {
-                Image(uiImage: foodImage)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: 200)
-                    .padding(.horizontal)
-            }
-        }
-        .background(Color(red: 80/255, green: 80/255, blue: 80/255).ignoresSafeArea())
-        .navigationBarItems(trailing:
-                                Button(action: {
-            // Add functionality for camera button
-        }) {
-            Image(systemName: "camera.fill")
-                .resizable()
-                .frame(width: 25, height: 20)
-                .foregroundColor(.white)
-                .padding()
-        })
-        
-        
-    }
+//struct LoggedInView: View {
+//    @State private var foodImage: UIImage?
+//    
+//    var body: some View {
+//        VStack {
+//            HStack {
+//                Image(systemName: "person.crop.circle.fill")
+//                    .resizable()
+//                    .frame(width: 40, height: 40)
+//                    .foregroundColor(.white)
+//                    .padding()
+//                Text("Cooking Together")
+//                    .font(.largeTitle)
+//                    .foregroundColor(.white)
+//                    .padding()
+//                Spacer()
+//                Image(systemName: "camera.fill")
+//                    .resizable()
+//                    .frame(width: 25, height: 30)
+//                    .foregroundColor(.white)
+//                    .padding()
+//                    .onTapGesture {
+//                        // Add functionality for camera button
+//                    }
+//            }
+//            .padding(.horizontal)
+//            .padding(.top, 20)
+//            
+//            ScrollView {
+//                VStack(spacing: 20) {
+//                    ForEach(0..<5) { index in
+//                        HStack {
+//                            
+//                            Image("FoodImgs")
+//                                .resizable()
+//                                .frame(width: 50, height: 50)
+//                                .foregroundColor(.white)
+//                                .padding()
+//                            VStack(alignment: .leading) {
+//                                Text("Cooking Event \(index)")
+//                                    .font(.title2)
+//                                    .fontWeight(.bold)
+//                                    .foregroundColor(.white)
+//                                Text("Join us for a fun evening of cooking and socializing with other foodies in the area! All skill levels welcome. Please let us know if you have any dietary restrictions or preferences.")
+//                                    .font(.body)
+//                                    .foregroundColor(.white)
+//                            }
+//                            Spacer()
+//                            Button(action: {
+//                                print("Join Event \(index) tapped")
+//                            }) {
+//                                Text("Join")
+//                                    .foregroundColor(.white)
+//                                    .padding(.horizontal, 10)
+//                                    .padding(.vertical, 5)
+//                                    .background(Color.blue)
+//                                    .cornerRadius(10)
+//                            }
+//                            .padding(.trailing, 10)
+//                        }
+//                        .frame(maxWidth: .infinity)
+//                        .background(Color(red: 50/255, green: 50/255, blue: 50/255))
+//                        .cornerRadius(10)
+//                    }
+//                }
+//                .padding(.horizontal)
+//                .padding(.bottom, 20)
+//            }
+//            
+//            if let foodImage = foodImage {
+//                Image(uiImage: foodImage)
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fit)
+//                    .frame(height: 200)
+//                    .padding(.horizontal)
+//            }
+//        }
+//        .background(Color(red: 80/255, green: 80/255, blue: 80/255).ignoresSafeArea())
+//        .navigationBarItems(trailing:
+//                                Button(action: {
+//            // Add functionality for camera button
+//        }) {
+//            Image(systemName: "camera.fill")
+//                .resizable()
+//                .frame(width: 25, height: 20)
+//                .foregroundColor(.white)
+//                .padding()
+//        })
+//        
+//        
+//    }
     
     
     
@@ -189,7 +189,7 @@ struct LoggedInView: View {
             .navigationTitle(dish.name)
         }
     }
-}
+
     
     struct LoginView: View {
         // MARK: Properties
